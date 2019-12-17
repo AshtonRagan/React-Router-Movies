37db575b7e86a2506b0b32c6629d9c10dfae24ca
ArkRaga <53240640+ArkRaga@users.noreply.github.com>
2019-12-17 16:16:40 -0500

    mvp met

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { browserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
 
-import SavedList from './Movies/SavedList';
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        Replace this Div with your Routes
+        <Switch>
+          <Route exact path="/">
+            <MovieList />
+          </Route>
+          <Route path="/movies/:id">
+            <Movie />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 };
